475193528b33e557f1c830eb5aa368675fe4836d
ofoe <ofoe_98@hotmail.co.uk>
2026-03-20 01:02:05 +0000

    added: images for slected projects

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ export default function About() {
           </div>
           <div className="about-imgs">
             <div className="aimg1" data-reveal="right"><img src="/images/company-souvenirs.webp" alt="Freetown Press branded corporate souvenirs" loading="lazy" /></div>
-            <div className="aimg2" data-reveal="right" data-delay="2"><img src="/images/workshop.webp" alt="Freetown Press workshop" loading="lazy" /></div>
+            <div className="aimg2" data-reveal="right" data-delay="2"><img src="/images/freetown-workshop.webp" alt="Freetown Press workshop" loading="lazy" /></div>
           </div>
         </div>
       </div>
